85b56d66a7b46016b36bd6624e3da596029358d3
root <aobersnel@apnex.com.au>
2020-09-17 09:13:59 +1000

    Updates again...

--- argo/argo.password.sh
+++ argo/argo.password.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 ## get pod name for password
-kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
+OLDPASS=$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2)
+NEWPASS='VMware1!SDDC'
+echo "Old Password [ $OLDPASS ] new password [ ${NEWPASS} ]"
 
 ## change password
-#argocd login 172.19.255.2 --insecure
-#argocd account update-password \
-#  --account <name> \
-#  --current-password <current-admin> \
-#  --new-password <new-user-password>
+argocd login 172.19.255.1 --insecure --username admin --password "${OLDPASS}"
+argocd account update-password --account admin --current-password "${OLDPASS}" --new-password "${NEWPASS}"
