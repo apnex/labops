0ebfbd7d528678aeb3958d73bbba680542ea10c0
root <aobersnel@apnex.com.au>
2020-09-18 17:26:39 +1000

    Updates...

--- argo/argo.password.sh
+++ argo/argo.password.sh
@@ -2,11 +2,16 @@
 
 NEWPASS=$1
 
+read -r -d '' FILTER <<-'EOF'
+	.status.loadBalancer.ingress[0].ip as $IP
+	| $IP + ":" + (.spec.ports[0].port|tostring)
+EOF
+export ARGOCD_SERVER=$(kubectl -n argocd get services vip-argocd-server -o json | jq -r "${FILTER}")
 if [[ -n $NEWPASS ]]; then
 	OLDPASS=$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2)
 	echo "Old Password [ $OLDPASS ] new password [ ${NEWPASS} ]"
 
 	## change password
-	argocd login 172.19.255.1 --insecure --username admin --password "${OLDPASS}"
+	argocd login ${ARGOCD_SERVER} --insecure --username admin --password "${OLDPASS}"
 	argocd account update-password --account admin --current-password "${OLDPASS}" --new-password "${NEWPASS}"
 fi
