2bb22fb74035fed6a8aec43c5eea5b99301445cd
root <aobersnel@apnex.com.au>
2020-09-19 18:21:32 +1000

    Updates...

--- rke/check.modules.sh
+++ rke/check.modules.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
-for MODULE in br_netfilter ip6_udp_tunnel ip_set ip_set_hash_ip ip_set_hash_net iptable_filter iptable_nat iptable_mangle iptable_raw nf_conntrack_netlink nf_conntrack nf_conntrack_ipv4 nf_defrag_ipv4 nf_nat nf_nat_ipv4 nf_nat_masquerade_ipv4 nfnetlink udp_tunnel veth vxlan x_tables xt_addrtype xt_conntrack xt_comment xt_mark xt_multiport xt_nat xt_recent xt_set xt_statistic xt_tcpudp;
-do
-  	if [[ ! $(lsmod | grep $MODULE) ]]; then
-                echo "module $MODULE is not present"
-	else
-                echo "module $MODULE is present"
-        fi
+read -r -d '' MODULELIST <<'EOF'
+[
+	"br_netfilter",
+	"ip6_udp_tunnel",
+	"ip_set",
+	"ip_set_hash_ip",
+	"ip_set_hash_net",
+	"iptable_filter",
+	"iptable_nat",
+	"iptable_mangle",
+	"iptable_raw",
+	"nf_conntrack_netlink",
+	"nf_conntrack",
+	"nf_conntrack_ipv4",
+	"nf_defrag_ipv4",
+	"nf_nat",
+	"nf_nat_ipv4",
+	"nf_nat_masquerade_ipv4",
+	"nfnetlink",
+	"udp_tunnel",
+	"veth",
+	"vxlan",
+	"x_tables",
+	"xt_addrtype",
+	"xt_conntrack",
+	"xt_comment",
+	"xt_mark",
+	"xt_multiport",
+	"xt_nat",
+	"xt_recent",
+	"xt_set",
+	"xt_statistic",
+	"xt_tcpudp"
+]
+EOF
+for MODULE in $(printf "${MODULELIST}" | jq -r '.[]'); do
+	if [[ ! lsmod | grep $MODULE ]]; then
+		echo "module $MODULE is not present";
+	fi;
 done
