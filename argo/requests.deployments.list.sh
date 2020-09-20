3c85123bacb1cdf276747cc47afaffeee5fd61ff
root <aobersnel@apnex.com.au>
2020-09-20 16:44:41 +1000

    Updates.. .

--- argo/requests.deployments.list.sh
+++ argo/requests.deployments.list.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
+
+NAMESPACE=${1}
 read -r -d '' FILTER <<-'EOF'
-	.items[] | .spec.template.spec.containers[0] |
-		{
-			"name": .name,
-			"resources": .resources
-		}
+	.
+	| del(.metadata.managedFields)
+	| del(.metadata.annotations."kubectl.kubernetes.io/last-applied-configuration")
+	| del(.metadata.annotations."deployment.kubernetes.io/revision")
+	| del(.metadata.creationTimestamp)
+	| del(.metadata.generation)
+	| del(.metadata.labels."app.kubernetes.io/instance")
+	| del(.metadata.resourceVersion)
+	| del(.metadata.selfLink)
+	| del(.metadata.uid)
+	| del(.spec.progressDeadlineSeconds)
+	| del(.spec.revisionHistoryLimit)
+	| del(.spec.strategy)
+	| del(.spec.template.metadata.creationTimestamp)
+	| del(.spec.template.spec.containers[0].terminationMessagePath)
+	| del(.spec.template.spec.containers[0].terminationMessagePolicy)
+	| del(.spec.template.spec.schedulerName)
+	| del(.spec.template.spec.securityContext)
+	| del(.spec.template.spec.terminationGracePeriodSeconds)
+	| del(.spec.template.spec.restartPolicy)
+	| del(.status)
 EOF
-read -r -d '' NEWFILTER <<-'EOF'
-	.spec.template.spec.containers[0].resources = {
-		"limits": {
-			"cpu": "300m",
-			"memory": "300Mi"
-		},
-		"requests": {
-			"cpu": "100m",
-			"memory": "100Mi"
-		}
-	}
-EOF
-kubectl -n sockshop get deployments -o json | jq --tab "${FILTER}"
+
+DEPLOYMENTS=$(kubectl -n "${NAMESPACE}" get deployments -o json)
+IFS=$'\n'
+for DEPA in $(echo "${DEPLOYMENTS}" | jq -c '.items[]'); do
+	echo "${DEPA}" | jq --tab "${FILTER}"
+done
+
+#kubectl -n sockshop get deployments orders -o json | jq --tab "${FILTER}"
