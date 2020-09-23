6e4490b898097e4119bbf275d071d1bc1400727d
root <aobersnel@apnex.com.au>
2020-09-23 22:58:47 +1000

    Updates..

--- rke/rke.install.sh
+++ rke/rke.install.sh
@@ -12,8 +12,9 @@ sleep 1
 cat /dev/zero | ssh-keygen -q -N "" >/dev/null
 yes | \cp ~/.ssh/id_rsa.pub /home/rke/.ssh/authorized_keys
 chown -R rke:docker /home/rke
-DOCKERVER=$(ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" rke@localhost docker version)
-echo "${DOCKERVER}"
+ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" rke@localhost << EOT
+	docker version
+EOT
 
 echo "### install rke cli ###"
 sleep 1
