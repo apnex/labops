e473ce5622ebbba4663beef8c9d6a83e96e0d193
root <aobersnel@apnex.com.au>
2020-09-23 22:42:56 +1000

    Updates..

--- rke/rke.install.sh
+++ rke/rke.install.sh
@@ -10,7 +10,7 @@ chmod -R go= /home/rke/.ssh
 echo "### create and copy ssh keys for rke user ###"
 sleep 1
 cat /dev/zero | ssh-keygen -q -N "" >/dev/null
-cp ~/.ssh/id_rsa.pub /home/rke/.ssh/authorized_keys
+yes | \cp ~/.ssh/id_rsa.pub /home/rke/.ssh/authorized_keys
 chown -R rke:docker /home/rke
 DOCKERVER=$(ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" rke@localhost docker version)
 echo "${DOCKERVER}"
