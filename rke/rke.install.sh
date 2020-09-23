dd1f3295e58753184b6d03bf9b8afd88f7044a66
root <aobersnel@apnex.com.au>
2020-09-23 22:15:01 +1000

    Updates...

--- rke/rke.install.sh
+++ rke/rke.install.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-## create rke service account
+echo "### create rke service account ###"
+sleep 1
 useradd -m -g docker rke
 mkdir -p /home/rke/.ssh
 chmod 700 /home/rke/.ssh
 chmod -R go= /home/rke/.ssh
 
-## create and copy ssh keys for rke user
+echo "### create and copy ssh keys for rke user ###"
+sleep 1
 cat /dev/zero | ssh-keygen -q -N "" >/dev/null
 cp ~/.ssh/id_rsa.pub /home/rke/.ssh/authorized_keys
 chown -R rke:docker /home/rke
 ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" rke@localhost docker version
 
-## install rke cli
+echo "### install rke cli ###"
+sleep 1
 curl -Lo /usr/local/bin/rke https://github.com/rancher/rke/releases/download/v1.2.0-rc10/rke_linux-amd64
 chmod +x /usr/local/bin/rke
 rke --version
 
-## install kubectl cli
+echo "### install kubectl cli ###"
+sleep 1
 KUBERELEASE=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
 curl -Lo /usr/local/bin/kubectl "https://storage.googleapis.com/kubernetes-release/release/${KUBERELEASE}/bin/linux/amd64/kubectl"
 chmod +x /usr/local/bin/kubectl
 kubectl version --client
 
-## initialise rke
+echo "### initialise rke cluster ###"
+sleep 1
 cat << EOF > ./rke.config.yaml
 nodes:
   - address: localhost
@@ -41,6 +46,7 @@ network:
 EOF
 rke up --config ./rke.config.yaml
 
-## copy kubeconfig
+echo "### sync kubeconfig ###"
+sleep 1
 mkdir -p $HOME/.kube
 cp kube_config_rke.config.yaml ~/.kube/config
