9d7360786f782eb5caaed49de29a8e94faedec61
root <aobersnel@apnex.com.au>
2020-09-23 22:56:39 +1000

    Updates..

--- rke/rke.test.sh
+++ rke/rke.test.sh
@@ -5,5 +5,7 @@ sleep 1
 cat /dev/zero | ssh-keygen -q -N "" >/dev/null
 yes | \cp ~/.ssh/id_rsa.pub /home/rke/.ssh/authorized_keys
 chown -R rke:docker /home/rke
-DOCKERVER=$(ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" rke@localhost docker version)
-echo "${DOCKERVER}"
+ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" rke@localhost << EOT
+	docker version
+EOT
+echo "FINISHED TEST"
