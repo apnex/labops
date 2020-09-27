d1893ecad0f8437fa751b6fefa3655e9eacd6b76
root <aobersnel@apnex.com.au>
2020-09-27 20:53:02 +1000

    Updates...

--- rke/runonce.sh
+++ rke/runonce.sh
@@ -4,7 +4,13 @@ if [[ -e /tmp/runonce ]]; then
 	rm /tmp/runonce
 	exec > /root/runonce.log 2>&1
 	curl -fsSL http://labops.sh/docker/install | sh
+	echo "Completed Evolution: Stage 1"
 	curl -fsSL http://labops.sh/rke/install | sh
+	echo "Completed Evolution: Stage 2"
+	curl -fsSL http://labops.sh/storage/install | sh
+	curl -fsSL http://labops.sh/metallb/install | sh
+	curl -fsSL http://labops.sh/metallb/prepare | sh
+	echo "Completed Evolution: Stage 3"
 fi
 
 exit
