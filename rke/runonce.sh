3e49d426ee899781d7c8fbbe33eae3850bb8e64a
root <aobersnel@apnex.com.au>
2020-09-28 14:52:00 +1000

    Updates..

--- rke/runonce.sh
+++ rke/runonce.sh
@@ -5,8 +5,17 @@ if [[ -e /tmp/runonce ]]; then
 	exec > /root/runonce.log 2>&1
 	curl -fsSL http://labops.sh/docker/install | sh
 	echo "Completed Evolution: Stage 1"
+
 	curl -fsSL http://labops.sh/rke/install | sh
 	echo "Completed Evolution: Stage 2"
+
+	HEALTHY=$(kubectl -n kube-system get pods 2>/dev/null)
+	while [[ -z ${HEALTHY} ]]; do
+		echo "socket [ localhost:6443 ] api [ no response ]"
+		sleep 10
+		HEALTHY=$(kubectl -n kube-system get pods 2>/dev/null)
+	done
+	echo "socket [ localhost:6443 ] api [ healthy ]"
 	curl -fsSL http://labops.sh/storage/install | sh
 	curl -fsSL http://labops.sh/metallb/install | sh
 	curl -fsSL http://labops.sh/metallb/prepare | sh
