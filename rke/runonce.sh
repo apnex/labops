1c1d103b2cd13dcc17b91b5a391953905f932f09
root <aobersnel@apnex.com.au>
2020-09-28 19:09:36 +1000

    Updates..

--- rke/runonce.sh
+++ rke/runonce.sh
@@ -9,6 +9,7 @@ if [[ -e /tmp/runonce ]]; then
 	curl -fsSL http://labops.sh/rke/install | sh
 	echo "Completed Evolution: Stage 2"
 
+	export KUBECONFIG=/root/.kube/config
 	curl -fsSL http://labops.sh/storage/install | sh
 	curl -fsSL http://labops.sh/metallb/install | sh
 	curl -fsSL http://labops.sh/metallb/prepare | sh
