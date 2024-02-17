1056c09fb306aba7560afa42059360f23761da83
root <aobersnel@apnex.com.au>
2024-02-17 18:10:47 +1100

    Updates..

---
+++ k3s/runonce.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ -e /tmp/runonce ]]; then
+	rm /tmp/runonce
+	exec &> >(tee -a /root/startup.log)
+	curl -fsSL http://labops.sh/docker/install | sh
+	echo "[[[ Completed Evolution: Stage 1 ]]]"
+
+	curl -fsSL http://labops.sh/k3s/install | sh
+	echo "[[[ Completed Evolution: Stage 2 ]]]"
+
+	export KUBECONFIG=/root/.kube/config
+	curl -fsSL http://labops.sh/storage/install | sh
+	curl -fsSL http://labops.sh/metallb/install | sh
+	curl -fsSL http://labops.sh/metallb/prepare | sh
+	echo "[[[ Completed Evolution: Stage 3 ]]]"
+	echo "1" > /root/startup.done
+fi
+
+exit
